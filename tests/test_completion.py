930650eb83594706275c17607566690c9e012690
ch0mler <tpgardner19@gmail.com>
2020-12-12 14:58:24 -0700

    added completion detail tests

--- tests/test_completion.py
+++ tests/test_completion.py
@@ -11,9 +11,13 @@ from yarals.base import protocol
 @pytest.mark.asyncio
 async def test_code_completion_regular(test_rules, yara_server):
     ''' Ensure code completion works with functions defined in modules schema '''
-    actual = []
     code_completion = str(test_rules.joinpath("code_completion.yara").resolve())
-    expected = sorted(["network", "registry", "filesystem", "sync"])
+    expected = [
+        protocol.CompletionItem("network", protocol.CompletionItemKind.CLASS, detail="cuckoo.network"),
+        protocol.CompletionItem("registry", protocol.CompletionItemKind.CLASS, detail="cuckoo.registry"),
+        protocol.CompletionItem("filesystem", protocol.CompletionItemKind.CLASS, detail="cuckoo.filesystem"),
+        protocol.CompletionItem("sync", protocol.CompletionItemKind.CLASS, detail="cuckoo.sync")
+    ]
     file_uri = helpers.create_file_uri(code_completion)
     message = {
         "params": {
@@ -21,12 +25,9 @@ async def test_code_completion_regular(test_rules, yara_server):
             "position": {"line": 10, "character": 15}
         }
     }
-    result = await yara_server.provide_code_completion(message, True)
-    assert len(result) == len(expected)
-    for completion in result:
-        assert isinstance(completion, protocol.CompletionItem)
-        actual.append(completion.insertText)
-    assert sorted(actual) == expected
+    actual = await yara_server.provide_code_completion(message, True)
+    assert len(actual) == len(expected)
+    assert actual == expected
 
 @pytest.mark.asyncio
 async def test_code_completion_overflow(test_rules, yara_server):
@@ -60,11 +61,15 @@ async def test_code_completion_unexpected(test_rules, yara_server):
 async def test_code_completion_module_dictionary(test_rules, yara_server):
     ''' Ensure code completion returns a properly-formatted list of strings when a module entry is a list of dictionary keys '''
     actual = []
+    expected = []
     options = [
         "Comments", "CompanyName", "FileDescription", "FileVersion", "InternalName",
         "LegalCopyright", "LegalTrademarks", "OriginalFilename", "ProductName", "ProductVersion"
     ]
-    expected = sorted(map(lambda option: f"version_info[\"{option}\"]", options))
+    for option in options:
+        snippet = "version_info[\"{}\"]".format(option)
+        detail = "pe.{}".format(snippet)
+        expected.append(protocol.CompletionItem(option, protocol.CompletionItemKind.INTERFACE, detail=detail, insertText=snippet))
     code_completion = str(test_rules.joinpath("code_completion.yara").resolve())
     file_uri = helpers.create_file_uri(code_completion)
     message = {
@@ -73,17 +78,16 @@ async def test_code_completion_module_dictionary(test_rules, yara_server):
             "position": {"line": 18, "character": 15}
         }
     }
-    result = await yara_server.provide_code_completion(message, True)
-    assert len(result) == len(expected)
-    for completion in result:
-        assert isinstance(result[0], protocol.CompletionItem)
-        actual.append(completion.insertText)
-    assert sorted(actual) == expected
+    actual = await yara_server.provide_code_completion(message, True)
+    assert len(actual) == len(expected)
+    assert actual == expected
 
 @pytest.mark.asyncio
 async def test_code_completion_module_method(test_rules, yara_server):
     ''' Ensure code completion returns a properly-formatted snippet string when a module entry is a method '''
-    expected_snippet = "is_dll()"
+    expected = [
+        protocol.CompletionItem("is_dll", protocol.CompletionItemKind.METHOD, detail="pe.is_dll()", insertText="is_dll()")
+    ]
     code_completion = str(test_rules.joinpath("code_completion.yara").resolve())
     file_uri = helpers.create_file_uri(code_completion)
     message = {
@@ -93,6 +97,5 @@ async def test_code_completion_module_method(test_rules, yara_server):
         }
     }
     result = await yara_server.provide_code_completion(message, True)
-    assert len(result) == 1
-    assert isinstance(result[0], protocol.CompletionItem)
-    assert result[0].insertText == expected_snippet
+    assert len(result) == len(expected)
+    assert result == expected
