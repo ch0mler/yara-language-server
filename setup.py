3256c4dff6d8adeb69e70b05942171402b4ae14f
ch0mler <tpgardner19@gmail.com>
2021-01-08 11:18:32 -0700

    added apache license

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ VERSION="0.0.3"
 classifiers = [
     'Development Status :: 3 - Alpha',
     'Programming Language :: Python :: 3.7',
+    'License :: OSI Approved :: Apache Software License',
 ]
 entry_points = {
     "console_scripts": [
@@ -18,22 +19,25 @@ entry_points = {
     ]
 }
 
-with open("README.md", "r") as long_description:
-    setup(
-        author="ch0mler",
-        author_email="thomas@infosec-intern.com",
-        classifiers=classifiers,
-        description=DESCRIPTION,
-        download_url="https://github.com/ch0mler/yara-language-server",
-        entry_points=entry_points,
-        long_description=long_description.read(),
-        long_description_content_type="text/markdown",
-        name="yara-language-server",
-        packages=find_packages(),
-        package_data={"yarals": ["data/*.json"]},
-        provides=["yarals"],
-        python_requires=">=3.7",
-        tests_require=["pytest", "pytest-asyncio", "pytest-timeout"],
-        url="https://ch0mler.github.io/yara-language-server/",
-        version=VERSION,
-    )
+with open("README.md", "r", encoding="utf-8") as readme:
+    long_description = readme.read()
+
+setup(
+    author="ch0mler",
+    author_email="thomas@infosec-intern.com",
+    classifiers=classifiers,
+    description=DESCRIPTION,
+    download_url="https://github.com/ch0mler/yara-language-server",
+    entry_points=entry_points,
+    license='Apache 2.0',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    name="yara-language-server",
+    packages=find_packages(),
+    package_data={"yarals": ["data/*.json"]},
+    provides=["yarals"],
+    python_requires=">=3.7",
+    tests_require=["pytest", "pytest-asyncio", "pytest-timeout"],
+    url="https://ch0mler.github.io/yara-language-server/",
+    version=VERSION,
+)
